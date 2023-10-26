cc163de21d4a11b6510e728a4ddbc88165ebebee
Nikita3141 <nikita.ingilizyan@gmail.com>
2023-10-26 19:40:05 +0300

    Test

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,12 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component("dog")
+public class Dog extends Animal {
+
+    public String toString (){
+        return "I am dog";
+    }
+
+}
